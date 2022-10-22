1876853535245351a00b5315d90d0ce625615358
woodroww <example528@gmail.com>
2022-10-22 10:52:41 -0400

    two sprites

--- src/main.rs
+++ src/main.rs
@@ -36,13 +36,13 @@ pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
 pub const RESOLUTION: f32 = 16.0 / 9.0;
 
 fn main() {
-    let height = 900.0;
+    // pygame WIDTH, HEIGHT = 900, 500
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(CLEAR))
         .insert_resource(WindowDescriptor {
-            width: height * RESOLUTION,
-            height,
+            width: 900.0,
+            height: 500.0,
             title: "Bevy Tutorial".to_string(),
             present_mode: bevy::window::PresentMode::AutoVsync,
             resizable: false,
@@ -54,28 +54,43 @@ fn main() {
 }
 
 fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn().
-        insert_bundle(SpriteBundle {
+
+    // let spaceship_size = (55, 40);
+    // original image 500 × 413
+
+    // red = pygame.Rect(x, y, width, height)
+    // red = pygame.Rect(700, 300, SPACESHIP_SIZE[0], SPACESHIP_SIZE[1])
+    // yellow = pygame.Rect(100, 300, SPACESHIP_SIZE[0], SPACESHIP_SIZE[1])
+    
+    // pygame window WIDTH, HEIGHT = 900, 500
+
+    let red_transform = Transform {
+        rotation: Quat::from_rotation_z(-90.0 * core::f32::consts::PI / 180.0),
+        scale: Vec3 { x: 0.1, y: 0.1, z: 1.0 },
+        translation: Vec3 { x: 250.0, y: 0.0, z: 1.0 }
+    };
+
+    commands.spawn().insert_bundle(SpriteBundle {
         texture: asset_server.load("spaceship_red.png"),
+        transform: red_transform,
         ..default()
     });
+
+    let yellow_transform = Transform {
+        rotation: Quat::from_rotation_z(90.0 * core::f32::consts::PI / 180.0),
+        scale: Vec3 { x: 0.1, y: 0.1, z: 1.0 },
+        translation: Vec3 { x: -250.0, y: 0.0, z: 1.0 }
+    };
+
+    commands.spawn().insert_bundle(SpriteBundle {
+        texture: asset_server.load("spaceship_yellow.png"),
+        transform: yellow_transform,
+        ..default()
+    });
+
+
 }
 
 fn spawn_camera(mut commands: Commands) {
-
     commands.spawn().insert_bundle(Camera2dBundle::default());
-
-   /* let mut camera = OrthographicCameraBundle::new_2d();
-    
-    camera.orthographic_projection.top = 1.0;
-    camera.orthographic_projection.bottom = -1.0;
-
-    camera.orthographic_projection.right = 1.0 * RESOLUTION;
-    camera.orthographic_projection.left = -1.0 * RESOLUTION;
-
-    camera.orthographic_projection.scaling_mode = ScalingMode::None;
-
-    commands.spawn_bundle(camera);
-    */
 }
-
