243c03bf377681d6264964a9d20d47bc64521ade
woodroww <example528@gmail.com>
2022-10-22 12:36:05 -0400

    ships move within their respective spaces

--- src/main.rs
+++ src/main.rs
@@ -36,13 +36,14 @@ pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
 pub const RESOLUTION: f32 = 16.0 / 9.0;
 
 fn main() {
-    // pygame WIDTH, HEIGHT = 900, 500
+    let width = 900.0;
+    let height = 500.0;
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(CLEAR))
         .insert_resource(WindowDescriptor {
-            width: 900.0,
-            height: 500.0,
+            width,
+            height,
             title: "Bevy Tutorial".to_string(),
             present_mode: bevy::window::PresentMode::AutoVsync,
             resizable: false,
@@ -51,51 +52,87 @@ fn main() {
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_player)
         .add_system(keyboard_input_system)
+        .add_system(toggle_override)
+        .add_system(change_scale_factor)
         .run();
 }
 
-fn keyboard_input_system(keyboard_input: Res<Input<KeyCode>>, mut ships: Query<(&mut Ship, &mut Transform)>) {
+fn keyboard_input_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut ships: Query<(&mut Ship, &mut Transform)>,
+    windows: ResMut<Windows>,
+) {
     /*info!("ships: {}", ships.iter().collect::<Vec<&Ship>>().len());
     for ship in ships.iter() {
         info!(ship.color);
     }*/
 
     let velocity = 5.0;
+    
+    let window = windows.primary();
+    let width = window.width(); 
+    let height = window.height();  
+    //info!("window w: {}, h: {}", width, height);
+
     for (ship, mut transform) in &mut ships {
+        // original image 500 × 413
+        let ship_width = 500.0 * transform.scale.x;
+        let ship_height = 413.0 * transform.scale.y;
+
         if ship.color == "yellow" {
             // up
-            if keyboard_input.pressed(KeyCode::E) {
+            if keyboard_input.pressed(KeyCode::E)
+                && transform.translation.y + velocity + ship_height / 2.0 < height / 2.0
+            {
                 transform.translation.y += velocity;
             }
             // down
-            if keyboard_input.pressed(KeyCode::D) {
+            if keyboard_input.pressed(KeyCode::D)
+                && transform.translation.y - velocity - ship_height / 2.0 > -height / 2.0
+            {
                 transform.translation.y -= velocity;
             }
             // right
-            if keyboard_input.pressed(KeyCode::F) {
+            if keyboard_input.pressed(KeyCode::F)
+                && transform.translation.x + velocity + (ship_width / 2.0) < 0.0
+            {
                 transform.translation.x += velocity;
             }
             // left
-            if keyboard_input.pressed(KeyCode::S) {
+            if keyboard_input.pressed(KeyCode::S)
+                && transform.translation.x - velocity - (ship_width / 2.0) > -width / 2.0
+            {
                 transform.translation.x -= velocity;
             }
         }
         if ship.color == "red" {
             // up
-            if keyboard_input.pressed(KeyCode::I) {
+            if keyboard_input.pressed(KeyCode::I)
+                && transform.translation.y + velocity + ship_height / 2.0 < height / 2.0
+            {
                 transform.translation.y += velocity;
+                info!("x: {}, y: {}", transform.translation.x, transform.translation.y);
             }
             // down
-            if keyboard_input.pressed(KeyCode::K) {
+            if keyboard_input.pressed(KeyCode::K)
+                && transform.translation.y - velocity - ship_height / 2.0 > -height / 2.0
+            {
                 transform.translation.y -= velocity;
+                info!("x: {}, y: {}", transform.translation.x, transform.translation.y);
             }
             // right
-            if keyboard_input.pressed(KeyCode::L) {
+            if keyboard_input.pressed(KeyCode::L)
+                && transform.translation.x + velocity + ship_width / 2.0 < width / 2.0
+            {
                 transform.translation.x += velocity;
+                info!("x: {}, y: {}", transform.translation.x, transform.translation.y);
             }
             // left
-            if keyboard_input.pressed(KeyCode::J) {
+            if keyboard_input.pressed(KeyCode::J)
+                && transform.translation.x - velocity - (ship_width / 2.0) > 0.0
+            {
                 transform.translation.x -= velocity;
+                info!("x: {}, y: {}", transform.translation.x, transform.translation.y);
             }
         }
     }
@@ -167,4 +204,22 @@ fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn().insert_bundle(Camera2dBundle::default());
+}
+
+/// This system toggles scale factor overrides when enter is pressed
+fn toggle_override(input: Res<Input<KeyCode>>, mut windows: ResMut<Windows>) {
+    let window = windows.primary_mut();
+    if input.just_pressed(KeyCode::Return) {
+        window.set_scale_factor_override(window.scale_factor_override().xor(Some(1.)));
+    }
+}
+
+/// This system changes the scale factor override when up or down is pressed
+fn change_scale_factor(input: Res<Input<KeyCode>>, mut windows: ResMut<Windows>) {
+    let window = windows.primary_mut();
+    if input.just_pressed(KeyCode::Up) {
+        window.set_scale_factor_override(window.scale_factor_override().map(|n| n + 1.));
+    } else if input.just_pressed(KeyCode::Down) {
+        window.set_scale_factor_override(window.scale_factor_override().map(|n| (n - 1.).max(1.)));
+    }
 }
